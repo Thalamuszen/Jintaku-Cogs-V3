9d37528c9094b4301b9b635cf49b7f2b1484d2cc
Thalamuszen <thalamuszen@hotmail.com>
2020-10-03 11:34:01 +0100

    Update roleplay.py

--- roleplay/roleplay.py
+++ roleplay/roleplay.py
@@ -439,9 +439,6 @@ class Roleplay(BaseCog):
             ],
             "spank": [
                 "https://cdn.discordapp.com/attachments/670618563912007681/676924829546315776/SpankAshe.gif",
-                "https://cdn.discordapp.com/attachments/670618563912007681/677194935056728074/SpankWidow.gif",
-                "https://cdn.discordapp.com/attachments/670618563912007681/682311660068667424/SpankDva.gif",
-                "https://cdn.discordapp.com/attachments/670618563912007681/682311672400183364/SpankTracer.gif",
             ],
             "pat": [
                 "https://cdn.weeb.sh/images/r180y1Yvb.gif",
@@ -810,6 +807,10 @@ class Roleplay(BaseCog):
 
         author = ctx.message.author
         images = await self.config.spank()
+        
+        spank = await self.fetch_nekos_life(ctx, "spank")
+        images.extend(spank)
+        
         mn = len(images)
         i = randint(0, mn - 1)
 
