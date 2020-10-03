47b5fa940bebdee06d69a7f565a2b9fd3ea008be
Thalamuszen <thalamuszen@hotmail.com>
2020-10-03 12:02:01 +0100

    Update roleplay.py

--- roleplay/roleplay.py
+++ roleplay/roleplay.py
@@ -814,7 +814,7 @@ class Roleplay(BaseCog):
         nekos = await self.fetch_nekos_life_nsfw(ctx, "spank")
         images.extend(nekos)
         
-        mn = len(images)
+        mn = len(nekos)
         i = randint(0, mn - 1)
 
         # Build Embed
